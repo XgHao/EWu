d7b4a8ad1800a1ef481ac3e26eb34a28b10768ae
郑兴豪 <957553851@qq.com>
2019-04-19 18:03:06 +0800

    图片上传-Step1

---
+++ Ewu.WebUI/Controllers/UpLoadImgController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ewu.WebUI.Controllers
+{
+    public class UpLoadImgController : Controller
+    {
+        // GET: UpLoadImg
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Cover()
+        {
+            bool isSavedSuccessfully = true;
+            string fName = "";
+            //遍历文件
+            foreach (string fileName in Request.Files)
+            {
+                //获取文件对象
+                HttpPostedFileBase file = Request.Files[fileName];
+                //保存文件
+                fName = file.FileName;
+                //文件不为空，长度大于0
+                if (file != null && file.ContentLength > 0 && file.ContentLength < 2097152)
+                {
+                    //储存路径
+                    DirectoryInfo originalDirectory = new DirectoryInfo(string.Format("{0}images\\Treasure", Server.MapPath(@"\")));
+                    //组合字符串成新的路径
+                    string pathString = Path.Combine(originalDirectory.ToString(), "imagepath");
+                    //返回路径的文件名和扩展名
+                    var fileName1 = Path.GetFileName(file.FileName);
+                    //获取该路径是否已经存在
+                    bool isExists = Directory.Exists(pathString);
+                    //不存在则新建
+                    if (!isExists)
+                    {
+                        Directory.CreateDirectory(pathString);
+                    }
+                    //完整文件路径
+                    var path = string.Format("{0}\\{1}", pathString, file.FileName);
+                    //保存文件
+                    file.SaveAs(path);
+                }
+                else
+                {
+                    isSavedSuccessfully = false;
+                }
+            }
+            string msg = isSavedSuccessfully ? fName : "Error in saving file";
+            return Json(new { Message = msg });
+        }
+    }
+}
--- Ewu.WebUI/Scripts/Dropzone/InitDropzone.js
+++ Ewu.WebUI/Scripts/Dropzone/InitDropzone.js
@@ -1,22 +1,52 @@
-﻿Dropzone.options.dropzoneForm = {
-    maxFiles: 2,
+﻿var CoverCnt = 0;
+
+Dropzone.options.CoverDropzone = {
+
+    maxFiles:1,
+    //添加上传取消和删除预览图片的链接，默认不添加
+    addRemoveLinks: true,
+
+    //关闭自动上传功能，默认会true会自动上传
+    //也就是添加一张图片向服务器发送一次请求
+    autoProcessQueue: false,
+
+    //允许上传多个照片
+    uploadMultiple: true,
+
+    //每次上传的最多文件数，经测试默认为2，坑啊
+    //记得修改web.config 限制上传文件大小的节
+    parallelUploads: 1,
+
     init: function () {
-        this.on("maxfilesexceeded", function (data) {
-            var res = eval('(' + "超出限制了" + ')');
+        var submitButton = document.querySelector("#submit-all")
+        var myDropzone = this; // closure
+
+        submitButton.addEventListener("click", function () {
+            //手动上传所有图片
+            myDropzone.processQueue();
         });
+
+        //当添加图片后的事件，上传按钮恢复可用
         this.on("addedfile", function (file) {
-            var removeButton = Dropzone.createElement("<button class='btn btn-danger'>Remove file</button>");
+            //alert("getAcceptedFiles" + this.getAcceptedFiles().length + "\ngetRejectedFiles" + this.getRejectedFiles().length + "\ngetQueuedFiles" + this.getQueuedFiles().length + "\ngetUploadingFiles" + this.getUploadingFiles().length);
 
-            var _this = this;
+            if (this.getAcceptedFiles().length == 1) {
+                alert("目前已有" + this.getAcceptedFiles().length + "张图片，已超上限");
+                this.removeFile(file);
+            }
+            $("#submit-all").removeAttr("disabled");
+        });
 
-            removeButton.addEventListener("click", function (e){
-                e.preventDefault();
-                e.stopPropagation();
+        //当上传完成后的事件，接受的数据为JSON格式
+        this.on("complete", function (file) {
+            
+        });
 
-                _this.removeFile(file);
-            });
-
-            file.previewElement.appendChild(removeButton);
+        //删除图片的事件，当上传的图片为空时，使上传按钮不可用状态
+        this.on("removedfile", function () {
+            if (this.getAcceptedFiles().length === 0) {
+                $("#submit-all").attr("disabled", true);
+            }
         });
     }
-}
+};
